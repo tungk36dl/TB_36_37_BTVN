18818c550e497f5bcdc9ebaafd66e781f516af2c
Pham The Tung <tungxeco55@gmail.com>
2024-02-24 00:00:28 +0700

    Btvn 23/02

---
+++ src/com/bai2.java
@@ -0,0 +1,11 @@
+package com;
+
+import java.util.Scanner;
+
+public class bai2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        
+    }
+}
